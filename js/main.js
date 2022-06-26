12876c9905c1aeab75a10f53a56d778d1ba07d58
Towfik <tufikhasan05@gmail>
2022-06-26 14:03:34 +0600

    (client side setup completed)

---
+++ js/main.js
@@ -0,0 +1,13 @@
+(function ($) {
+  $(document).ready(function () {
+    $('.menu-bar').click(function () {
+      $('.main-menu').slideToggle();
+    });
+    $('.add_post_form_open_btn').click(function () {
+      $('.add-post-form-area').css('display', 'block');
+    });
+    $('.add_post_form_close_btn').click(function () {
+      $('.add-post-form-area').css('display', 'none');
+    });
+  });
+})(jQuery);
