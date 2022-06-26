cfc8cf6c9acdea92aaa99e2f718f18a9aedd5c76
Towfik <tufikhasan05@gmail>
2022-06-26 14:06:18 +0600

    mysqli database connection create

---
+++ includes/db.php
@@ -0,0 +1,13 @@
+<?php
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'codebook';
+// Create connection
+$conn =  new mysqli($server,$username,$password,$database);
+
+if ($conn->connect_error) {
+    die('Connection error: ' .$conn-connect_error());
+}else {
+    echo 'Connected successfully';
+}
