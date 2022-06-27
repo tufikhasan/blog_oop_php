dda2f71669b4f0caa989cbb686598b2fada6aa7d
Towfik <tufikhasan05@gmail>
2022-06-27 12:35:40 +0600

    add post form template added

--- profile.php
+++ profile.php
@@ -7,35 +7,7 @@ include('./template-part/main_header.php') ;
 $full_name = $_SESSION['first'].' '.$_SESSION['last'];
 ?>
     <!---Add post section start--->
-    <button class="primary_btn add_post_form_open_btn">&plus;</button>
-    <section class="add-post-form-area">
-        <button class="primary_btn add_post_form_close_btn">&cross;</button>
-        <div class="main-content">
-            <div class="form-area flex align-items-center justify-content-center" style="height: 100vh;">
-                <form action="">
-                    <h2>Create a new post your</h2>
-                    <div class="input-box">
-                        <label for="title">Post title <span>*</span></label>
-                        <input type="text" name="first_name" id="first_name" placeholder="Enter post title"
-                            style="width: 100%" required>
-                    </div>
-                    <div class="input-box">
-                        <label for="post-desc">Post Description <span>*</span></label>
-                        <textarea name="" id="post-desc" cols="30" rows="10" style="width: 100%"
-                            placeholder="Enter post Description" required></textarea>
-                    </div>
-                    <div class="input-box">
-                        <label for="post-img">Post image</label>
-                        <input type="file" id="post-img" style="width: 100%" />
-                    </div>
-                    <div class=" flex justify-content-space-between">
-                        <input class="primary_btn" type="submit" value="Add new post" />
-                        <button class="primary_btn" type="reset">&circlearrowright;</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </section>
+<?php include('./template-part/add_post_form.php') ?>
     <!---Add post section end--->
     <!---profile main section start--->
     <section>
