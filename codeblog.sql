487f6f084f438c29bd41a5bf782f2260029b15b4
Towfik <tufikhasan05@gmail>
2022-06-28 10:50:53 +0600

    export sql file for database structure

---
+++ codeblog.sql
@@ -0,0 +1,126 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 28, 2022 at 06:46 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `codeblog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_comment`
+--
+
+CREATE TABLE `user_comment` (
+  `id` int(11) NOT NULL,
+  `user_name` varchar(32) NOT NULL,
+  `user_pic` varchar(255) NOT NULL,
+  `comment` varchar(255) NOT NULL,
+  `comment_post_id` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_info`
+--
+
+CREATE TABLE `user_info` (
+  `id` int(6) UNSIGNED NOT NULL,
+  `firstname` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `birthday` varchar(20) DEFAULT NULL,
+  `profile_picture` varchar(255) NOT NULL,
+  `gender` varchar(30) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `reg_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_info`
+--
+
+INSERT INTO `user_info` (`id`, `firstname`, `lastname`, `email`, `birthday`, `profile_picture`, `gender`, `user_password`, `reg_date`) VALUES
+(15, 'Nadim', 'hasan', 'admin@gmail.com', '1997-12-05', 'profile.png', 'on', '827ccb0eea8a706c4c34a16891f84e7b', '2022-06-28 04:44:22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_post`
+--
+
+CREATE TABLE `user_post` (
+  `id` int(11) NOT NULL,
+  `user_name` varchar(32) NOT NULL,
+  `user_picture` varchar(255) NOT NULL,
+  `post_date` varchar(100) NOT NULL,
+  `post_title` varchar(255) NOT NULL,
+  `post_content` varchar(500) NOT NULL,
+  `post_image` varchar(400) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_comment`
+--
+ALTER TABLE `user_comment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_info`
+--
+ALTER TABLE `user_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_post`
+--
+ALTER TABLE `user_post`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_comment`
+--
+ALTER TABLE `user_comment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `user_info`
+--
+ALTER TABLE `user_info`
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `user_post`
+--
+ALTER TABLE `user_post`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
