d0602c59e5ae4941cac883c759f1783020316628
Towfik <tufikhasan05@gmail>
2022-07-05 15:27:42 +0600

    edit form sql syntax error fix

--- includes/edit.php
+++ includes/edit.php
@@ -5,8 +5,8 @@ include('db.php');
 if(isset($_GET['update'])){
 		
 	$update_id =$_GET['update'];
-	$edit_post_title = $_POST['edit-post-title'];
-    $edit_post_desc = $_POST['edit-post-desc'];
+	$edit_post_title = $conn->real_escape_string($_POST['edit-post-title']);
+    $edit_post_desc = $conn->real_escape_string($_POST['edit-post-desc']);
 	
 	$edit_post_image = $_FILES['edit-post-img']['name'];
 	$edit_post_tmp_n = $_FILES['edit-post-img']['tmp_name'];
@@ -18,7 +18,7 @@ if(isset($_GET['update'])){
 		exit();	
 	}else{
 		move_uploaded_file($edit_post_tmp_n,'../img/post/'.$edit_post_image);	
-		mysqli_query($conn,"UPDATE user_post SET post_title='$edit_post_title',post_content='$edit_post_desc',post_image='$edit_post_image' WHERE id='$update_id'");
+		mysqli_query($conn,"UPDATE user_post SET `post_title`='".$edit_post_title."',`post_content`='".$edit_post_desc."',post_image='$edit_post_image' WHERE id='$update_id'");
 		//echo '<script type="text/javascript">alert("post update successfully")</script>';	
 		//echo'<script type="text/javascript">window.open("../profile.php","_self")</script>';
         header('location: ../profile.php?result=post-update-successfully');		
